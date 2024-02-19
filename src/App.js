29aace05419f26af697a697bc8fe0f263a845b38
Charlese Muchmore <charlymuchmore@gmail.com>
2024-02-19 13:13:11 -0800

    created basic footer component

--- src/App.js
+++ src/App.js
@@ -8,19 +8,13 @@ function App() {
     return (
         <div classname="App">
             <header className="App-header">
-                <p>
-                Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                Learn React
-                </a>
-            </header>
             <Routes>
                 <Route className="App-link" path="/" element={<Layout />}>
                 <Route index element={<Home />} />
                 <Route path="*" element={<NoPage />} />
                 </Route>             
             </Routes>
+            </header>
         </div>
     )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+// import {NavLink} from 'react-router-dom'; 
+import "./Footer.css";
+
+function Footer() {
+
+    return (
+    <div className="footer">
+        <div className="footerLogo">
+            Logo
+        </div>
+        <div className="socialLinkIcon">
+            LinkedIn
+        </div>
+        <div className="socialLinkIcon">
+            Email
+        </div>
+        <div className="socialLinkIcon">
+            Trailhead
+        </div>
+        <div className="socialLinkIcon">
+            Facebook
+        </div>
+        <div className="socialLinkIcon">
+            Instagram
+        </div>
+    </div>
+    )
+}
+
+export default Footer;
