8d507467abb23198e3f197fb8798a830fd7c2988
Charlese Muchmore <charlymuchmore@gmail.com>
2024-02-19 22:13:39 -0800

    added first row to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,34 @@
+import "./Home.css";
+import me from '../images/menoback2.jpg';
+
+
 function Home() {
     return (
-    <div>
-        welcome home!
+    <div className="container-fluid">
+        <div className="row bg-info">
+            <div className="col-sm-3 col-md-4 col-lg-6 m-auto">
+                <h1>Hello there!</h1>
+            </div>
+            <div className="col-sm-9 col-md-8 col-lg-6 ">
+            <img className="me" alt="charlese" src={me}></img>
+            </div>
+        </div>
+        <div className="row bg-primary">
+            <div className="col-sm-3 col-md-4 col-lg-6 m-auto">
+                <h1>Hello there!</h1>
+            </div>
+            <div className="col-sm-9 col-md-8 col-lg-6 ">
+            <img className="me" alt="charlese" src={me}></img>
+            </div>
+        </div>
+        <div className="row bg-secondary">
+            <div className="col-sm-3 col-md-4 col-lg-6 m-auto">
+                <h1>Hello there!</h1>
+            </div>
+            <div className="col-sm-9 col-md-8 col-lg-6 ">
+            <img className="me" alt="charlese" src={me}></img>
+            </div>
+        </div>
     </div>
     )
 }
